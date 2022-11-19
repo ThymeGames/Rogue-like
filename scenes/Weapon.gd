4f7f08f7943afcd63020f5a6d95fbfc6da586283
Andrey Sobolev <chorzoww@gmail.com>
2022-11-20 01:42:16 +0400

    Attacking with sword added

---
+++ scenes/Weapon.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+onready var _animation_player = $AnimationPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func animate():
+	_animation_player.play("attack_stronk")
