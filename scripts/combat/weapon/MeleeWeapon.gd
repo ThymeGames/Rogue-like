8c79e7098afece51440a9b5aaca0a6962fc78da2
Andrey Pikunov <pikunov.andrew@gmail.com>
2022-12-03 01:06:23 +0400

    hitbox is collider, need to fix

--- scripts/combat/weapon/MeleeWeapon.gd
+++ scripts/combat/weapon/MeleeWeapon.gd
@@ -3,9 +3,15 @@ class_name MeleeWeapon
 
 export(float, 0.0, 6.28) var slash_length = 2.0  # radians
 export(float, 0.0, 1.0) var attack_duration = 0.25  # seconds
-export(float, 0.0, 32.0) var length = 16.0 # pixels
+export(float, 0.0, 32.0) var length = 16.0  # pixels
+export (float, 2.0, 10.0) var width = 4.0  # pixels
 
 
 func _ready() -> void:
+    
     $Cooldown.wait_time = attack_duration
     $Cooldown.stop()
+
+    var hitbox : CollisionShape2D = $Hitbox
+    hitbox.position = Vector2(length / 2.0, 0.0);
+    hitbox.shape.extents = Vector2(length / 2.0, width / 2.0);
